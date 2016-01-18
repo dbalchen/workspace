cb2a6b5fc0ea936728212b7976b4c4a86f2a2e1a
David Balchen <dave@balchen.org>
2016-01-18 17:03:46 -0600

    looks good

--- MpsMonitors/src/sql/switch_behindCBB.sql
+++
@@ -1,8 +0,0 @@
-SELECT FSRC_SENSOR_ID,
-       TO_CHAR(MAX(system_rcv_date), 'DD-MON-YYYY HH:MI:SS AM') 
-FROM AC_LOGICAL_FILES 
-WHERE FSRC_TYPE_ID in ('AAA') and  (system_rcv_date > sysdate - 10)
-GROUP BY FSRC_SENSOR_ID
-HAVING MAX(system_rcv_date) < sysdate - (?/24) --$self->{hours_behind}/24) 
-and MAX(NEW_TIME(system_rcv_date,'GMT',?)) < sysdate - (?/24)
---                                     #tzstring        #hours_behind 
