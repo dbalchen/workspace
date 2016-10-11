23c6bc421c090d0a6f90a7d79ba8ee124aa14a9e
David Balchen <dave@balchen.org>
2016-10-11 16:38:57 -0500

    Outcollects Done

---
+++ volteRoaming/src/bin/getFileInfoOutcollectsData.pl
@@ -0,0 +1,70 @@
+#! /usr/local/bin/perl
+
+use DBI;
+
+#Test parameters remove when going to production.
+#$ARGV[0] = "20161003";
+
+# For test only.....
+my $ORACLE_HOME = "/usr/lib/oracle/12.1/client/";
+my $ORACLE_SID  = "bodsprd";
+$ENV{ORACLE_HOME} = $ORACLE_HOME;
+$ENV{ORACLE_SID}  = $ORACLE_SID;
+$ENV{PATH}        = "$ENV{PATH}:$ORACLE_HOME/bin";
+
+
+my $dbconn = getBRMPRD();
+
+$sql = "select count(*), t2.clearinghouse, sum(t1.amount) as REVENUE, sum(message_accounting_digits) as DATA_VOLUME 
+         from data_outcollect t1, roaming_partner t2 where TRIM(REGEXP_REPLACE(t1.PARTNER,',')) = TRIM(REGEXP_REPLACE(t2.PARTNER,',')) 
+         and t1.process_date = to_date('$ARGV[0]','YYYYMMDD') group by t2.clearinghouse";
+
+$sth = $dbconn->prepare($sql);
+$sth->execute() or sendErr();
+
+my $filename = 'DATA_CIBER_'.$ARGV[0];
+my $reportFile = $filename.'.rpt.csv';
+open( RPT, ">$reportFile" ) || errorExit("Could not open log file.... OutCollect!!!!");
+
+while (my @rows = $sth->fetchrow_array() ) {
+  
+  print RPT $rows[1]."\t".$rows[0]."\t".$rows[2]."\t".$rows[3]."\n";
+
+}
+close(RPT);
+
+
+$sql = "select TRIM(REGEXP_REPLACE(t1.PARTNER,',')) as PARTNER, t1.settlement_date,t2.clearinghouse, sum(t1.amount) as REVENUE, sum(message_accounting_digits) as DATA_VOLUME 
+         from data_outcollect t1, roaming_partner t2 where TRIM(REGEXP_REPLACE(t1.PARTNER,',')) = TRIM(REGEXP_REPLACE(t2.PARTNER,',')) 
+         and t1.process_date = to_date('$ARGV[0]','YYYYMMDD') group by TRIM(REGEXP_REPLACE(t1.PARTNER,',')),t2.clearinghouse, t1.settlement_date order by 1,2";
+
+$sth = $dbconn->prepare($sql);
+$sth->execute() or sendErr();
+
+my $reportFile = $filename.'.partner.csv';
+open( RPT, ">$reportFile" ) || errorExit("Could not open log file.... OutCollect!!!!");
+
+while (my @rows = $sth->fetchrow_array() ) {
+  
+  print RPT $rows[0]."\t".$rows[1]."\t".$rows[2]."\t".$rows[3]."\t".$rows[4]."\n";
+
+}
+
+close(RPT);
+
+$dbconn->disconnect();
+
+
+exit(0);
+
+sub getBRMPRD {
+
+  #	my $dbPwd = "BODSPRD_INVOICE_APP_EBI";
+  #	$dbods = (DBI->connect("DBI:Oracle:$dbPwd",,));
+  my $dbods = DBI->connect( "dbi:Oracle:brmprd", "md1dbal1", "Reptar500#" );
+  unless ( defined $dbods ) {
+    sendErr();
+  }
+  return $dbods;
+}
+
--- volteRoaming/src/bin/roamingReconciliation.pl
+++ volteRoaming/src/bin/roamingReconciliation.pl
@@ -8,10 +8,10 @@ use Spreadsheet::WriteExcel;
 use MIME::Lite;
 
 #Test parameters remove when going to production.
-$ARGV[0] = "SDIRI_FCIBER,SDATACBR_FDATACBR,CIBER_CIBER";
+$ARGV[0] = "SDIRI_FCIBER,SDATACBR_FDATACBR,CIBER_CIBER,DATA_CIBER";
 #$ARGV[0] = "SDATACBR_FDATACBR";
 #$ARGV[0] = "CIBER_CIBER";
-
+#$ARGV[0] = "DATA_CIBER";
 $ENV{'REC_HOME'} = '/home/dbalchen/workspace/volteRoaming/src/bin';
 
 # Setup Initial variables
@@ -30,15 +30,17 @@ $dirs{'CIBER_CIBER'} = '/pkgbl02/inf/aimsys/prdwrk2/var/usc/projs/apr/interfaces
 $jobs{'SDIRI_FCIBER'} = 'getFileInfo.pl';
 $jobs{'SDATACBR_FDATACBR'} = 'getFileInfoData.pl';
 $jobs{'CIBER_CIBER'} = 'getFileInfoOutcollects.pl';
+$jobs{'DATA_CIBER'} = 'getFileInfoOutcollectsData.pl';
 
 $headings{'SDIRI_FCIBER'} = ['File Name','Identifier','Total Records','Total Charges','Dropped Records','Duplicates','Sent To TC','Rejected','Rejected Total','APRM Records','APRM Total'];
 $headings{'SDATACBR_FDATACBR'} = ['File Name','Identifier','Total Records','Total Charges','Dropped Records','Sent To TC','Rejected','Rejected Total','APRM Records','APRM Total'];
 $headings{'CIBER_CIBER'} = ['File Name','Total Records','Total Charges','APRM Records','APRM Total'];
+$headings{'DATA_CIBER'} = ['Clearinghouse','Total Records','Revenue','Data Volume'];
 
 $tab{'SDIRI_FCIBER'} = "Voice Incollect";
 $tab{'SDATACBR_FDATACBR'} = "Data Incollect";
 $tab{'CIBER_CIBER'} = 'Voice Outcollect';
-
+$tab{'DATA_CIBER'} = 'Data Outcollect';
 # Get Roaming switches to check
 
 my @switches = split(',', $ARGV[0]);
@@ -52,28 +54,33 @@ $workbook = Spreadsheet::WriteExcel->new($excel_file);
 # Get Roaming files
 foreach my $switch (@switches) {
 
-  my $hh = 'find '.$dirs{$switch}.' -name "' . $switch . '*' . $timeStamp . '*" -print |';
+  if ($switch ne "DATA_CIBER") {   
+    my $hh = 'find '.$dirs{$switch}.' -name "' . $switch . '*' . $timeStamp . '*" -print |';
 
-  if ( !open( FINDLIST, "$hh" ) ) {
-    errorExit("Cannot create FINDLIST: $!\n");
-  }
-
-  while ( my $filename = <FINDLIST> ) {
-    chomp($filename);
-
-    $hh = "$ENV{'REC_HOME'}/$jobs{$switch} $filename &";
-
-    system($hh);
-    my $tproc = getTotalProc();
-    while ($tproc > $max_process ) {
-      sleep 10;
-      $tproc = getTotalProc()
+    if ( !open( FINDLIST, "$hh" ) ) {
+      errorExit("Cannot create FINDLIST: $!\n");
     }
-  }
 
-  $hh = "$ENV{'REC_HOME'}/getFileInfoAprm.pl $switch $timeStamp &";
-  system($hh);
-  
+    while ( my $filename = <FINDLIST> ) {
+      chomp($filename);
+
+      $hh = "$ENV{'REC_HOME'}/$jobs{$switch} $filename &";
+
+      system($hh);
+      my $tproc = getTotalProc();
+      while ($tproc > $max_process ) {
+	sleep 10;
+	$tproc = getTotalProc()
+      }
+    }
+
+    $hh = "$ENV{'REC_HOME'}/getFileInfoAprm.pl $switch $timeStamp &";
+    system($hh);
+  } else {
+
+    $hh = "$ENV{'REC_HOME'}/$jobs{$switch} $timeStamp &";
+    system($hh);
+  }
   sleep 10;
   $tproc = getTotalProc();
   while ($tproc > 0) {
@@ -82,17 +89,23 @@ foreach my $switch (@switches) {
   
   createExcel($timeStamp,$switch,"rpt",$headings{$switch},$tab{$switch});
 
-  if ($switch ne "CIBER_CIBER") {
+  if (($switch ne "CIBER_CIBER") && ($switch ne "DATA_CIBER")) {
     my $heading = ['File Name','Error Code','Airtime Charge'];
 
     my $rejectTab = "Rejected ".$tab{$switch};
     createExcel($timeStamp,$switch,"err",$heading,$rejectTab);
   }
-   
-  $heading = ['Carrier Code','BP Start Date','Record Count','Usage Sum','Sum Amount'];
-  $rejectTab = $tab{$switch}." APRM";
-  createExcel($timeStamp,$switch,"arpm",$heading,$rejectTab);
-  
+
+  if ($switch ne "DATA_CIBER") {
+    $heading = ['Carrier Code','BP Start Date','Record Count','Usage Sum','Sum Amount'];
+    $rejectTab = $tab{$switch}." APRM";
+    createExcel($timeStamp,$switch,"arpm",$heading,$rejectTab);
+  } else {
+    $heading = ['Partner','Settlement_Date','Clearinghouse','Revenue','Data_Volume'];
+    $rejectTab = $tab{$switch}." by Partner";
+    createExcel($timeStamp,$switch,"partner",$heading,$rejectTab);
+  }
+      
   $hh = "rm $switch".'*csv';
   system("$hh");
   
