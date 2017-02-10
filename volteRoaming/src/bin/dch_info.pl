3d42d3666a34d7098f800ee27748daeba5fd2307
David Balchen <dave@balchen.org>
2017-02-10 17:15:09 -0600

    This week's changes

--- volteRoaming/src/bin/dch_info.pl
+++ volteRoaming/src/bin/dch_info.pl
@@ -3,61 +3,88 @@
 #### Test Only
 #$ARGV[0] = 'CIBER_CIBER_20170115144213_3036793_0002.dat.done';
 
-my $file = $ARGV[0];
-my $date = substr($file,index($file,"_20")+1,8);
-my $short_date = substr($date,2);
+my $file       = $ARGV[0];
+my $dch_file   = $ARGV[1];
+my $date       = substr( $file, index( $file, "_T2" ) + 1, 8 );
+my $short_date = substr( $date, 2 );
 
-my $process_date = substr($date,0,4).'-'.substr($date,4,2).'-'.substr($date,6,2);
+my $process_date =
+    substr( $date, 0, 4 ) . '-'
+  . substr( $date, 4, 2 ) . '-'
+  . substr( $date, 6, 2 );
 
-$hh = "cat $file ".'|'." grep '^98' ".'|'." sort -u ".'|'." grep '^98".$short_date."' ".'|';
+$hh =
+    "cat $file " . '|'
+  . " grep '^98' " . '|'
+  . " sort -u " . '|'
+  . " grep '^98"
+  . $short_date . "' " . '|';
 
-my $fileout = $date."_differences.csv";
+my $fileout = $date . "_differences.csv";
 
-open(OUT,">>$fileout") or exit(1);
+open( OUT, ">>$fileout" ) or exit(1);
 
-open(PIPE,$hh) or exit(1);
+open( PIPE, $hh ) or exit(1);
 
-while (my $buff = <PIPE>) {
-  chomp($buff);
-  my $totrecs = substr($buff,21,4); $totrecs =~ s/^0+//g;
-  my $totcharges = substr($buff,25,12)/100;  $totcharges = sprintf("%.2f", $totcharges);
- 
-  my $mygrep = substr($buff,16,5)."\t".substr($buff,11,5)."\t"."0".substr($buff,8,3)."\t".$totrecs."\t".$totcharges;
+while ( my $buff = <PIPE> ) {
+    chomp($buff);
+    my $totrecs = substr( $buff, 21, 4 );
+    $totrecs =~ s/^0+//g;
+    my $totcharges = substr( $buff, 25, 12 ) / 100;
+    $totcharges = sprintf( "%.2f", $totcharges );
 
-  $hh = "grep '$mygrep' OutcollectDCH_test.csv";
+    my $mygrep =
+        substr( $buff, 16, 5 ) . "\t"
+      . substr( $buff, 11, 5 ) . "\t" . "0"
+      . substr( $buff, 8,  3 ) . "\t"
+      . $totrecs . "\t"
+      . $totcharges;
 
-  my @sniff = `$hh`;
+    $hh = "grep '$mygrep' $dch_file";
 
-  if (@sniff == 0) {
-    my $mygrep2 = substr($buff,16,5)."\t".substr($buff,11,5)."\t"."0".substr($buff,8,3)."\t".$totrecs;
-    $hh = "grep '$mygrep2' OutcollectDCH_test.csv";
+    my @sniff = `$hh`;
 
-    my @sniffsniff = `$hh`;
+    if ( @sniff == 0 ) {
+        my $mygrep2 =
+            substr( $buff, 16, 5 ) . "\t"
+          . substr( $buff, 11, 5 ) . "\t" . "0"
+          . substr( $buff, 8,  3 ) . "\t"
+          . $totrecs;
+        $hh = "grep '$mygrep2' $dch_file ";
 
-    if (@sniffsniff >= 1) {
-      
-      print OUT "Charge Difference -- USCC: "."\t".$mygrep."\n";
-      print OUT "Charge Difference -- Syniverse: ".$sniffsniff[0];
-      
-    } elsif (@sniffsniff == 0) {
-      
-      my $mygrep3 = substr($buff,16,5)."\t".substr($buff,11,5)."\t"."0".substr($buff,8,3);
-      $hh = "grep '$mygrep3' OutcollectDCH_test.csv";
-      
-      my @sniffsniffsniff = `$hh`;
+        my @sniffsniff = `$hh`;
 
-      if (@sniffsniffsniff >= 1) {
-	
-	print OUT "Record Difference - Rejects -- USCC: "."\t".$mygrep."\n";
-	print OUT "Record Difference - Rejects -- Syniverse: ".$sniffsniffsniff[0];
-	
-      } else {
-	
-	print OUT "Block Rejected: ".$mygrep."\n";
-	
-      }
+        if ( @sniffsniff >= 1 ) {
+
+            print OUT "Charge Difference -- USCC: " . "\t" . $mygrep . "\n";
+            print OUT "Charge Difference -- Syniverse: " . $sniffsniff[0];
+
+        }
+        elsif ( @sniffsniff == 0 ) {
+
+            my $mygrep3 =
+                substr( $buff, 16, 5 ) . "\t"
+              . substr( $buff, 11, 5 ) . "\t" . "0"
+              . substr( $buff, 8,  3 );
+            $hh = "grep '$mygrep3' $dch_file";
+
+            my @sniffsniffsniff = `$hh`;
+
+            if ( @sniffsniffsniff >= 1 ) {
+
+                print OUT "Record Difference - Rejects -- USCC: " . "\t"
+                  . $mygrep . "\n";
+                print OUT "Record Difference - Rejects -- Syniverse: "
+                  . $sniffsniffsniff[0];
+
+            }
+            else {
+
+                print OUT "Block Rejected: " . $mygrep . "\n";
+
+            }
+        }
     }
-  }
 
 }
 
---
+++ volteRoaming/src/bin/dch_infoCount.pl
@@ -0,0 +1,49 @@
+#! /usr/bin/perl
+
+my $file       = $ARGV[0];
+my $dch_file   = $ARGV[1];
+
+my $total_records = 0;
+my $total_charge = 0;
+my $total_volume = 0;
+
+my %serSidSeq = {};
+
+open(DCH,"< $dch_file") || exit(1);
+
+while(my $buff = <DCH>)
+{
+    chomp($buff);
+    my @vec = split(/\t/,$buff);
+    my $key = $vec[2].$vec[3].$vec[4];
+    $serSidSeq{$key} = $vec[7]."\t".$vec[8]."\t".$vec[9];
+}
+
+my $hh =  "cat $file " . '|'. " grep '^98' " . '|' . " sort -u " . '|';
+
+open( PIPE, $hh ) or exit(1);
+
+while ( my $buff = <PIPE> ) {
+    chomp($buff);
+
+#    my $mykey = substr( $buff, 16, 5 ).substr( $buff, 11, 5 ) ."0".substr( $buff, 8,  3 );
+    my $mykey = substr( $buff, 11, 5 ).substr( $buff, 16, 5 ) ."0".substr( $buff, 8,  3 );
+
+    my $result = $serSidSeq{$mykey};
+
+    my($records,$charges,$volumes) = split(/\t/,$result);
+
+    $total_records = $total_records + $records;
+    $total_charges = $total_charges + $charges;
+    $total_volume = $total_volume + $volumes;
+
+
+}
+
+print "$total_records\n";
+print "$total_charges\n";
+print "$total_volume\n";
+
+close(OUT);
+
+exit(0);
