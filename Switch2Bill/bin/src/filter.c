f77d83c755fb9aaea3191d2c6eb7c512d77f51c2
David Balchen <dave@balchen.org>
2017-03-02 16:54:57 -0600

    This week changes

---
+++ Switch2Bill/bin/src/filter.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : filter.c
+ Author      : David Balchen
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello From Dave"); /* prints Hello From Daveland */
+	return EXIT_SUCCESS;
+}
