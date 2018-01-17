e12ad020ea150a7cc732e97708757b37f5b22547
David Balchen <dave@balchen.org>
2018-01-17 17:17:04 -0600

    aprm_staging is mostly done

--- volteRoaming/src/bin/load_staging.pl
+++ volteRoaming/src/bin/load_staging.pl
@@ -10,7 +10,7 @@ $ENV{ORACLE_HOME} = $ORACLE_HOME;
 $ENV{ORACLE_SID}  = $ORACLE_SID;
 $ENV{PATH}        = "$ENV{PATH}:$ORACLE_HOME/bin";
 
-$ARGV[0] = '20171201';
+$ARGV[0] = '20171101';
 $ARGV[1] = "APRM";
 my @reports = split( ',', $ARGV[1] );
 
@@ -288,7 +288,7 @@ if ( substr( $date, 6, 2 ) eq '01' ) {
 	@aprmArray = (
 		'LTE',
 		'DISP_RM',
-		'NLDLT' #,
+		'NLDLT'#,
 #		'CDMA_A_IN_VOICE',
 #		'CDMA_A_IN_DATA',
 #		'CDMA_A_OUT_VOICE',
@@ -376,7 +376,7 @@ sub loadAprm {
 		print "$sqldel\n";
 
 		$conn2 = $dbconnb->prepare($sqldel);
-#		$conn2->execute() or sendErr();
+		$conn2->execute() or sendErr();
 
 		my $sql = $sqls{$wsql};
 
