41360631226b0b0be74e09a7a27377f15acf9df5
David Balchen <dave@balchen.org>
2018-01-25 17:08:51 -0600

    This weeks changes

--- volteRoaming/src/bin/load_staging.pl
+++ volteRoaming/src/bin/load_staging.pl
@@ -10,7 +10,7 @@ $ENV{ORACLE_HOME} = $ORACLE_HOME;
 $ENV{ORACLE_SID}  = $ORACLE_SID;
 $ENV{PATH}        = "$ENV{PATH}:$ORACLE_HOME/bin";
 
-$ARGV[0] = '20171101';
+$ARGV[0] = '20171201';
 $ARGV[1] = "APRM";
 my @reports = split( ',', $ARGV[1] );
 
@@ -286,13 +286,13 @@ my @aprmArray = ();
 if ( substr( $date, 6, 2 ) eq '01' ) {
 
 	@aprmArray = (
-		'LTE',
-		'DISP_RM',
-		'NLDLT'#,
+#		'LTE',
+#		'DISP_RM',
+#		'NLDLT',
 #		'CDMA_A_IN_VOICE',
 #		'CDMA_A_IN_DATA',
 #		'CDMA_A_OUT_VOICE',
-#		'CDMA_A_OUT_DATA'
+		'CDMA_A_OUT_DATA'
 	);
 
 }
